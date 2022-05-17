061cc07e72764821c958fe18671df521fa33f8df
Asirini Hapuarachchi <87674946+it20604226@users.noreply.github.com>
2022-05-18 00:29:05 +0530

    Add files via upload

---
+++ AddActivity.java
@@ -0,0 +1,34 @@
+package com.book.now.app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class AddActivity extends AppCompatActivity {
+
+    EditText name_input, agent_input, code_input;
+    Button add_button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add);
+
+        name_input = findViewById(R.id.name_input);
+        agent_input = findViewById(R.id.agent_input);
+        code_input = findViewById(R.id.code_input);
+        add_button = findViewById(R.id.add_button);
+        add_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                MyDatabaseHelper myDB = new MyDatabaseHelper(AddActivity.this);
+                myDB.addBook(name_input.getText().toString().trim(),
+                        agent_input.getText().toString().trim(),
+                        Integer.valueOf(code_input.getText().toString().trim()));
+            }
+        });
+    }
+}
